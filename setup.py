76ee54ab2d38ec5cd13b4ae1ea78db70f7e85f0a
Aaron Weimann <weimann@hhu.de>
2015-11-06 14:26:54 +0100

    changed numpy requirement to 1.10.1; modified MANIFEST.in to recursively include data dir

--- setup.py
+++ setup.py
@@ -9,7 +9,5 @@ setup(name='traitar',
         license='GNU General Public License, version 3 (GPL-3.0)',
         packages=['traitar'],
         scripts = ['traitar/traitar', 'traitar/merge_preds.py', 'traitar/heatmap.py', 'traitar/domtblout2gene_generic.py', 'traitar/predict.py', 'traitar/hmmer2filtered_best.py'],
-        #package_data = {"traitar": ["data/sample_data/traitar_out/phenotype_prediction/phypat+GGL/*", "data/sample_data/traitar_out/phenotype_prediction/phypat/phypat/*",  "data/sample_data/traitar_out/phenotype_prediction/*", "data/sample_data/traitar_out/*", "data/sample_data/*", "data/*", "data/models/*", "data/sample_data/traitar_out/pfam_annotation/*"]},
-        include_package_data = True,
         zip_safe=False,
-        install_requires = ["pandas >= 0.14.1", "matplotlib >= 1.0", "scipy >= 0.9.0", "numpy >= 1.7"])
+        install_requires = ["pandas >= 0.14.1", "matplotlib >= 1.0", "scipy >= 0.9.0", "numpy >= 1.10.1"])
