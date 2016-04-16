82879be4fc5f5ed19c5b3a8dbb8565a53b8c8881
Aaron Weimann <weimann@hhu.de>
2016-04-17 00:36:26 +0200

    Phenotype prediction and merging modified
    - prediction score tables no longer generated
    - integrated PhenotypeCollection class
    - implemented merging prediction tables when the phentoypes are diffent across the models

--- traitar/merge_preds.py
+++ traitar/merge_preds.py
@@ -11,53 +11,38 @@ def flatten_df(df1, df2, out):
 
 def comb_preds(phypat_dir, phypat_GGL_dir, out_dir, k):
     #phypat only predictions
-    m1_raw = ps.read_csv("%s/predictions_raw.txt"%phypat_dir, index_col = 0, sep = "\t")
-    m1 = ps.DataFrame(ps.np.zeros((m1_raw.shape[0], m1_raw.shape[1] / 5)))
-    m1.index = m1_raw.index
-    for i in range(m1_raw.shape[1] / 5):
-        m1.iloc[:, i] = m1_raw.iloc[:, i*5 : i*5 + 5].apply(lambda x: (x > 0).sum(), axis = 1 )
-    m1_scores = ps.read_csv("%s/predictions_majority-vote_mean-score.txt"%phypat_dir, index_col = 0, sep = "\t")
+    m1_maj = ps.read_csv("%s/predictions_majority-vote.txt"%phypat_dir, index_col = 0, sep = "\t")
+    m1 = ps.read_csv("%s/predictions_single-votes.txt"%phypat_dir, index_col = 0, sep = "\t")
+    m2_maj = ps.read_csv("%s/predictions_majority-vote.txt"%phypat_GGL_dir, index_col = 0, sep = "\t")
+    m2 = ps.read_csv("%s/predictions_single-votes.txt"%phypat_GGL_dir, index_col = 0, sep = "\t")
     #write to disk a single vote version of the predictions
-    m1.columns = m1_scores.columns
-    m1.index = m1_scores.index
-    m1.to_csv("%s/predictions_single-votes.txt"%phypat_dir, sep = "\t")
     #phypat plus ml predictions
-    m2_raw = ps.read_csv("%s/predictions_raw.txt"%phypat_GGL_dir, index_col = 0,  sep = "\t")
-    m2 = ps.DataFrame(ps.np.zeros((m2_raw.shape[0], m2_raw.shape[1] / 5)))
-    m2.index = m2_raw.index
-    #aggregate single predictor outcomes
-    for i in range(m2_raw.shape[1] / 5):
-        m2.iloc[:, i] = m2_raw.iloc[:, i*5 : i*5 + 5].apply(lambda x: (x > 0).sum(), axis = 1 )
-    m2_scores = ps.read_csv("%s/predictions_majority-vote_mean-score.txt"%phypat_GGL_dir, index_col = 0, sep = "\t")
-    #write to disk a single vote version of the predictions
-    m2.index = m2_scores.index
-    m2.columns = m2_scores.columns
-    m2.to_csv("%s/predictions_single-votes.txt"%phypat_GGL_dir, sep = "\t")
     #set NAs to zero
     m1[ps.isnull(m1)] = 0
     m2[ps.isnull(m2)] = 0
-    m1_scores[ps.isnull(m1_scores)] = 0
-    m2_scores[ps.isnull(m2_scores)] = 0
-    #generate majority vote matrices
-    m1_maj = m1.copy()
-    m2_maj = m2.copy()
-    m1_maj[m1 < (k/2 + 1)] = 0
-    m1_maj[m1 >= (k/2 + 1)] = 1
-    m2_maj[m2 < (k/2 + 1)] = 0
-    m2_maj[m2 >= (k/2 + 1)] = 1
     #collect phenotypes that are shared / different in the two data sets
     #combine predictions of phypat and phypat+GGL into one matrix 
-    if ps.np.all(m1.columns == m2.columns):
-        m = m1 + m2 
-        m_maj = m1_maj.copy()
-        m_maj[m2_maj == 1] = 2
-        m_maj[(m1_maj == 1) & (m2_maj == 1)] = 3
-        #m_maj = ((m1_maj + m2_maj) > 0).astype('int')
-        m_scores = (m1_scores +  m2_scores)/2
-        #write named majority, single vote and mean score version of combined, phypat and phypat+GGL 
-        m.to_csv("%s/predictions_single-votes_combined.txt"%out_dir, sep = "\t")
-        m_maj.to_csv("%s/predictions_majority-vote_combined.txt"%out_dir, index_label = None, sep = "\t")
-        m_scores.to_csv("%s/predictions_majority-vote_mean-score_combined.txt"%out_dir, index_label = None, sep = "\t")
+    m_columns = set(m1.columns).union(set(m2.columns))
+    m = ps.DataFrame(ps.np.zeros((m1.shape[0], len(m_columns))))
+    m_maj = ps.DataFrame(ps.np.zeros((m1.shape[0], len(m_columns))))
+    m_maj.columns = m.columns = m_columns
+    m_maj.index = m1.index
+    m.index = m1.index
+    #combine single voters
+    m.loc[:, m1.columns] = m1
+    m.loc[:, m2.columns] = m.loc[:, m2.columns] + m2
+    #combine majority votes
+    m_maj.loc[:, set(m1.columns).difference(set(m2.columns))] = m1_maj.loc[:, set(m1.columns).difference(set(m2.columns))] * 2
+    m_maj.loc[:, set(m2.columns).difference(set(m1.columns))] = m2_maj.loc[:, set(m2.columns).difference(set(m1.columns))] 
+    m_temp = m_maj.loc[:, set(m2.columns).intersection(set(m1.columns))]
+    #sys.stderr.write(str((m1_maj.loc[:, set(m2.columns).intersection(set(m1.columns))] == 1) & (m2_maj.loc[:, set(m2.columns).intersection(set(m1.columns))] == 1)))
+    if not set(m2.columns).intersection(set(m1.columns)) == set():
+        m_temp[(m1_maj.loc[:, set(m2.columns).intersection(set(m1.columns))] == 1) & (m2_maj.loc[:, set(m2.columns).intersection(set(m1.columns))] == 1)] = 3
+        m_temp[(m1_maj.loc[:, set(m2.columns).intersection(set(m1.columns))] == 1) & (m2_maj.loc[:, set(m2.columns).intersection(set(m1.columns))] == 0)] = 1
+        m_temp[(m1_maj.loc[:, set(m2.columns).intersection(set(m1.columns))] == 0) & (m2_maj.loc[:, set(m2.columns).intersection(set(m1.columns))] == 1)] = 2
+        m_maj.loc[:, set(m2.columns).intersection(set(m1.columns))] = m_temp
+    m.to_csv("%s/predictions_single-votes_combined.txt"%out_dir, sep = "\t")
+    m_maj.to_csv("%s/predictions_majority-vote_combined.txt"%out_dir, index_label = None, sep = "\t")
     flatten_df(m1_maj, m2_maj, "%s/predictions_flat_majority-votes_combined.txt"%out_dir)
     flatten_df(m1, m2, "%s/predictions_flat_single-votes_combined.txt"%out_dir)
 
