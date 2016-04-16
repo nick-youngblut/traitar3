3118ab348c2b320add6dedc7531e67e9a51170d9
Aaron Weimann <weimann@hhu.de>
2016-04-17 00:19:03 +0200

    Added class PhenotypeCollection
    - provides access to the phenotype archive tar

---
+++ traitar/PhenotypeCollection.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import tarfile
+class PhenotypeCollection:
+    def __init__(self, archive_f):
+        self.archive_f = archive_f
+        self.tar = tarfile.open(archive_f, mode = "r:gz")
+        info = self.tar.extractfile("config.txt")
+        info_df = pd.read_csv(info, sep = "\t", index_col = 0)
+        self.name = info_df.loc["archive_name", "value"]
+        self.hmm_f = info_df.loc["hmm_f", "value"]
+        self.hmm_name = info_df.loc["hmm_name", "value"]
+    def get_pt2acc(self):
+        pt2acc_f = self.tar.extractfile("pt2acc.txt")
+        pt2acc = pd.read_csv(pt2acc_f, sep = "\t", index_col = 0,  encoding='utf-8')
+        pt2acc.index = pt2acc.index.astype('U')
+        return pt2acc
+    def get_acc2pt(self):
+        pt2acc_f = self.tar.extractfile("pt2acc.txt")
+        pt2acc = pd.read_csv(pt2acc_f, sep = "\t", index_col = 1,  encoding='utf-8')
+        pt2acc.index = pt2acc.index.astype('U')
+        return pt2acc
+    def get_pf2desc(self):
+        pfam_mapping = pd.read_csv(self.tar.extractfile("pf2acc_desc.txt"), index_col = 0, sep = "\t")
+        return pfam_mapping
+    def get_name(self):
+        pt2desc_f = self.tar.extractfile("pt2acc.txt")
+        return self.name
+    def get_archive_f(self):
+        return self.archive_f
+    def get_hmm_f(self):
+        return self.hmm_f
+    def get_hmm_name(self):
+        return self.hmm_name
+    def get_bias(self, pt):
+        bias_f = self.tar.extractfile("%s_bias.txt"%(pt))
+        bias = pd.read_csv(bias_f, sep = "\t", index_col = 0, header = None)
+        return bias
+    def get_predictors(self, pt):
+        extracted_f = self.tar.extractfile("%s_feats.txt"%(pt))
+        predictors = pd.read_csv(extracted_f, sep = "\t",  index_col = 0 )
+        return predictors
+    def get_majority_features(self,pt):
+        extracted_f = self.tar.extractfile("%s_majority_features+weights.txt" % pt)
+        feats = pd.read_csv(extracted_f, sep = "\t", index_col = 0)
+        return feats
