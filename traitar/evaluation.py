16adfc6f260141f7bd8bedd33b351c22a74753b8
Aaron Weimann <weimann@hhu.de>
2016-05-23 16:58:46 +0200

    Added evaluation.py
    - implemented a sub program of traitar for comparing phenotype with a given gold standard
    - calculates macro and micro averaged evaluation measures and per phenotype measures
    - writes misclassified samples per phenotypes to disk
    - supports missing predictions in the gold standard
    - supports minimum number of positive or negatives samples per phenotypes for the macro accuracy calculation

---
+++ traitar/evaluation.py
@@ -0,0 +1,130 @@
+import pandas as pd
+import os
+import sys
+
+class evaluate:
+    
+    @staticmethod
+    def evaluate(out, gold_standard_f, traitar_pred_f, min_samples):
+        """compare traitar predictions with a given gold standard"""
+        #read in gold standard
+        gs = pd.read_csv(gold_standard_f, index_col = 0, sep = "\t")
+        #read in traitar preds
+        tp = pd.read_csv(traitar_pred_f, index_col = 0, sep = "\t")
+        #get pts that are in gold standard and in the pt models 
+        pts = list(set(gs.columns.tolist()).intersection(set(tp.columns.tolist())))
+        pt_gold_too_few_samples = gs.apply(lambda x: pd.Series(((x[~pd.isnull(x) & (x == 0)].sum() >= min_samples) & (x[~pd.isnull(x) & (x == 1)].sum() >= min_samples))))
+        if len(pts) == 0:
+            sys.exit("No phenotypes shared between traitar predictions and gold standard")
+        #confusion matrix per phenotype
+        conf_per_pt = pd.DataFrame(pd.np.zeros(shape = (len(pts), 4)))
+        conf_per_pt.index = pts
+        #performance measures per phenotype 
+        perf_per_pt = pd.DataFrame(pd.np.zeros(shape = (len(pts), 4)))
+        perf_per_pt.index = pts
+        perf_per_pt.columns = ["recall pos", "recall neg", "macro accuracy", "precision"]
+        for pt in pts:
+            conf_per_pt.loc[pt, ] = evaluate.confusion_m(gs.loc[:, pt], tp.loc[:, pt])
+            perf_per_pt.loc[pt, ] = evaluate.get_performance(conf_per_pt.loc[pt, ]) 
+            miscl = evaluate.get_miscl(gs.loc[:, pt], tp.loc[:, pt])
+            print miscl
+            if not len(miscl) == 0:
+                miscl_m = pd.concat([gs.loc[miscl, pt], tp.loc[miscl, pt]], axis = 1)
+                miscl_m.columns = ["gold standard", "traitar predictions"]
+                miscl_m.to_csv('%s.txt' % os.path.join(out, pt), sep = "\t")
+            
+        #sum up confusion
+        overall_conf = conf_per_pt.sum(axis = 0)
+        #micro averaged performance measures
+        micro_perf = evaluate.get_performance(overall_conf)
+        #macro averaged performance
+        macro_perf = perf_per_pt.mean(axis = 0)
+        #write to disk perf2pt and overall accuracy measures
+        perf_per_pt.to_csv(os.path.join(out, "perf_per_pt.txt"), sep = "\t")
+        res = pd.concat([micro_perf, macro_perf], axis = 1)
+        res.columns = ["micro",  "macro"]
+        res.to_csv(os.path.join(out, "perf_overall.txt"), sep = "\t")
+        #write to disk confusion matrix
+        conf_per_pt.columns = ["True negatives", "False positives", "False negatives", "True positives"] 
+        conf_per_pt.to_csv(os.path.join(out, "confusion_matrix_per_pt.txt"), sep = "\t")
+        
+  
+    @staticmethod
+    def get_miscl(y, y_pred):
+        """get misclassified samples""" 
+        #print y[y == 1]
+        #print y[y == 0]
+        #print y_pred[y == 1]
+        #print y_pred[y == 0]
+        #print y[y == 1] != y_pred[y == 1]
+        #print y[y == 0] != y_pred[y == 0]
+        FN =  y[y == 1].loc[y[y == 1] != y_pred[y == 1],].index
+        FP =  y[y == 0].loc[y[y == 0] != y_pred[y == 0], ].index
+        return FN.tolist() + FP.tolist()
+
+    @staticmethod 
+    def get_performance(conf):
+        """get different performance measures from a confusion matrix"""
+        recall_pos = evaluate.recall_pos_conf(conf)
+        recall_neg = evaluate.recall_neg_conf(conf)
+        bacc = evaluate.bacc(recall_pos, recall_neg)
+        prec = evaluate.precision_conf(conf)
+        s = pd.Series([recall_pos, recall_neg, bacc, prec])
+        s.index = ["recall pos", "recall neg", "macro accuracy", "precision"]
+        return s
+
+    @staticmethod 
+    def confusion_m(y,y_pred):
+        """get confusion matrix TN FP /n FN TP"""
+        TP = (y[y == 1] == y_pred[y == 1]).sum()
+        FN = (y[y == 1] != y_pred[y == 1]).sum()
+        TN = (y[y == 0] == y_pred[y == 0]).sum()
+        FP = (y[y == 0] != y_pred[y == 0]).sum()
+        return pd.np.array([TN, FP, FN, TP])
+
+    @staticmethod 
+    def recall_pos(y,y_pred):
+        """compute recall of the positive class"""
+        return (y[y == 1] == y_pred[y==1]).sum()/float((y==+1).sum())
+    
+    @staticmethod 
+    def recall_pos_conf(conf):
+        """compute recall of the positive class"""
+        TN, FP, FN, TP = conf
+        if (TP + FN) == 0:
+            float('nan') 
+        return TP/float(TP+FN)
+    
+    @staticmethod 
+    def recall_neg_conf(conf):
+        """compute recall of the positive class"""
+        TN, FP, FN, TP = conf
+        if (TN + FP) == 0:
+            return float('nan')
+        return TN/float(TN+FP)
+
+    @staticmethod 
+    def recall_neg(y, y_pred):
+        """compute recall of the negative class"""
+        return (y[y == 0] == y_pred[y==0]).sum()/float((y==0).sum())
+    
+    @staticmethod 
+    def precision(y, y_pred):
+        """compute precision"""
+        TP = (y[y == 1] == y_pred[y == 1]).sum()
+        FP = (y[y == 0] != y_pred[y == 0]).sum()
+        if (TP + FP) == 0:
+            return 0
+        return TP / float(TP + FP)   
+    @staticmethod 
+    def precision_conf(conf):
+        """compute precision"""
+        TN, FP, FN, TP = conf
+        if (TP + FP) == 0:
+            return float('nan')
+        return TP / float(TP + FP)
+
+    @staticmethod
+    def bacc(pos_acc, neg_acc):
+        """compute balanced accuracy"""
+        return float(pos_acc + neg_acc)/2
